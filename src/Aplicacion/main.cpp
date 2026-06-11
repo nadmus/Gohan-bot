056e9c4002bd004881ceab7b8b654f61c793576e
Nadir <mustafa.nadiragustin@gmail.com>
2026-06-11 12:09:53 -0300

    fix: fixed ticks count

--- src/Aplicacion/main.cpp
+++ src/Aplicacion/main.cpp
@@ -8,7 +8,7 @@ int main(){
 	SCTimer sct;
 	PWM pwm(sct, Pin::port0, 14, SCTimer::OUTPUT0);
 	pwm.SetPeriod(PWM::MILISEG, 0, 20);
-	pwm.SetTon(PWM::MICROSEG, 2500);
+	pwm.SetTon(PWM::MICROSEG, 500);
 	pwm.StartPWM();
 
 	while(1)
